0e3cfb57253c81d23590452280bd8727e1bec242
Omar-mk123 <139969084+Omar-mk123@users.noreply.github.com>
2026-08-21 15:12:10 +0300

    Create authRoutes.js

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const {
+    register,
+    login
+} = require("../controllers/authController");
+
+const validate = require("../middleware/validationMiddleware");
+
+const {
+    registerSchema,
+    loginSchema
+} = require("../validators/authValidator");
+
+const router = express.Router();
+
+// Register
+router.post(
+    "/register",
+    validate(registerSchema),
+    register
+);
+
+// Login
+router.post(
+    "/login",
+    validate(loginSchema),
+    login
+);
+
+module.exports = router;
